832a6ea0c6ef5b097f81febd7b54f9c9613b0785
Muthu Kumaran <mms@adobe.com>
2024-03-14 01:11:01 +0530

    copy dist files

--- react-app/webpack.config.js
+++ react-app/webpack.config.js
@@ -1,9 +1,39 @@
-// All this three lines bellow are importings
-const webpack = require('webpack');
+// const {Compilation} = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const copyfiles = require('copyfiles');
 const path = require('path');
+
+
+// Plugin to copy  dist files to EDS location
+class CopyFiles {
+  apply(compiler) {
+    compiler.hooks.done.tap('Copy', compilation => {
+      // console.log('compilation ===>');
+      // copy component files
+      copyfiles(
+        ['./dist/**/*', '../blocks'],
+        {
+          all: true,
+          up: 1,
+          exclude: ['./dist/vendor/**/*', './dist/index.html'],
+          verbose: true,
+        },
+        (err) => err && console.error(err)
+      );
+      // copy vendor file
+      copyfiles(
+        ['./dist/vendor/vendor.js', '../scripts'],
+        {
+          up: 2,
+        },
+        (err) => err && console.error(err)
+      );
+    });
+  }
+}
 
 // Here goes all configuration
 module.exports = {
@@ -26,18 +56,19 @@ module.exports = {
       },
       {
         test: /\.(sa|sc|c)ss$/, // styles files
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
       filename: '[name]/[name].css',
-      // chunkFilename: 'clientlib-chunks/resources/[name].[hash]-ltr.css',
     }),
     new HtmlWebpackPlugin({
       template: 'static/index.html', // create a template to start from
     }),
+    new CopyFiles(),
   ],
   optimization: {
     minimize: true,
@@ -51,25 +82,25 @@ module.exports = {
         },
       }),
     ],
-		chunkIds: "named",
-		splitChunks: {
-			cacheGroups: {
-				commons: {
-					chunks: "initial",
-					minChunks: 2,
-					maxInitialRequests: 5, // The default limit is too small to showcase the effect
-					minSize: 0 // This is example is too small to create commons chunks
-				},
-				vendor: {
-					test: /node_modules/,
-					chunks: "initial",
-					name: "vendor",
-					priority: 10,
-					enforce: true
-				}
-			}
-		}
-	},
+    chunkIds: 'named',
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          chunks: 'initial',
+          minChunks: 2,
+          maxInitialRequests: 5, // The default limit is too small to showcase the effect
+          minSize: 0, // This is example is too small to create commons chunks
+        },
+        vendor: {
+          test: /node_modules/,
+          chunks: 'initial',
+          name: 'vendor',
+          priority: 10,
+          enforce: true,
+        },
+      },
+    },
+  },
   devServer: {
     host: 'localhost', // where to run
     historyApiFallback: true,

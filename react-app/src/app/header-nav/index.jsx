9dfd36cf793d9efac8c75728be9ec7a6ce7ace2d
Muthu Kumaran <mms@adobe.com>
2024-03-16 22:46:19 +0530

    added observer for element waiting

--- react-app/src/app/header-nav/index.jsx
+++ react-app/src/app/header-nav/index.jsx
@@ -3,5 +3,21 @@ import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import './index.css';
 
-const root = createRoot(document.querySelector('.header-nav'));
-root.render(<App />);
+function onElementAvailable(selector, callback) {
+  const observer = new MutationObserver(mutations => {
+    const elem = document.querySelector(selector);
+    if (elem) {
+      observer.disconnect();
+      callback(elem);
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+}
+
+onElementAvailable('.header-nav', elem => {
+  if (elem) {
+    const root = createRoot(elem);
+    root.render(<App />);
+  }
+});

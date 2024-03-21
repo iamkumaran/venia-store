20f16a3db6a909b08f2a855425b9ef0b733221c1
Muthu Kumaran <mms@adobe.com>
2024-03-21 13:22:16 +0530

    CSS optimizing

--- react-app/webpack.config.js
+++ react-app/webpack.config.js
@@ -1,5 +1,6 @@
 // const {Compilation} = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -90,6 +91,26 @@ module.exports = {
           compress: {
             drop_console: true,
           },
+        },
+      }),
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            'default',
+            {
+              calc: true,
+              // convertValues: true,
+              discardComments: {
+                removeAll: true,
+              },
+              discardDuplicates: true,
+              discardEmpty: true,
+              mergeRules: true,
+              normalizeCharset: true,
+              // reduceInitial: true, // This is since IE11 does not support the value Initial
+              svgo: true,
+            },
+          ],
         },
       }),
     ],

98414bdb342b34aa1ec1139ef34ddf123d42ad9c
Muthu Kumaran <mms@adobe.com>
2024-04-01 11:58:12 +0530

    code updated

--- react-app/src/library/Error/APIFail.jsx
+++ react-app/src/library/Error/APIFail.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 
 const APIFail = ({ error }) => (
-  <div>
-    <p>Looks like GraphQL call failed. Please run this in your CMD/Terminal (Req: Node.js)</p>
-    <p>
-      <textarea readOnly>
-        npx local-cors-proxy --proxyUrl https://venia.magento.com/ --port 4000 --proxyPartial ""
-      </textarea>
-    </p>
-
-    <p>Oh no... {error?.message}</p>
+  <div style={{ fontSize: '12px', marginTop: '17px' }}>
+    Oh no... {error?.message} <br />
+    Looks like GraphQL call failed. Please run below proxy in CMD/Terminal (Req: Node.js) <br />
+    <textarea readOnly style={{ color: 'red', width: '100%', height: '32px', marginTop: '5px' }}>
+      npx local-cors-proxy --proxyUrl https://venia.magento.com/ --port 4000 --proxyPartial ""
+    </textarea>
   </div>
 );
 
